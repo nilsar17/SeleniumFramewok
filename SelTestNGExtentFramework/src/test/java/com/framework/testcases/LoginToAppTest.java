cab8db704656dd10aa01e4e382542f3351359d55
niranjankulkarni <nilsar17@gmail.com>
2022-02-05 10:42:17 +0000

    Fixed Maven issue updated pom and ran the project using IntelliJ

--- SelTestNGExtentFramework/src/test/java/com/framework/testcases/LoginToAppTest.java
+++ SelTestNGExtentFramework/src/test/java/com/framework/testcases/LoginToAppTest.java
@@ -9,24 +9,23 @@ import com.framework.pages.LoginPage;
 public class LoginToAppTest extends BaseClass{
     @Test
     public void loginApp() throws InterruptedException{
-        
+
         LoginPage lpage = new LoginPage(driver);
-        
+
         logger = report.createTest("Login To CRMPro");
         logger.info("Starting Application");
-        
+
         lpage.loginToApp(excel.getStringData("Sheet1", 0, 0), excel.getStringData("Sheet1", 0, 1));
-        
+
         logger.info("Started Application");
-        
+
         Thread.sleep(9000);
         HomePage hpage = new HomePage(driver);
         if (hpage.homeNav.isDisplayed()) {
-        	logger.pass("Login Done Successfully");
+            logger.pass("Login Done Successfully");
         }else {
-        	logger.fail("Failed to load home page");
+            logger.fail("Failed to load home page");
         }
-        
+
     }
 }
-
