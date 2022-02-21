2684caf23f50795fd54a2d2af761aceb656c0b6f
niranjankulkarni <nilsar17@gmail.com>
2022-02-21 23:38:37 +0000

    Made changes to populate test name in the ExtentReport

--- SelTestNGExtentFramework/src/test/java/com/framework/testcases/LoginToAppTest.java
+++ SelTestNGExtentFramework/src/test/java/com/framework/testcases/LoginToAppTest.java
@@ -1,6 +1,7 @@
 package com.framework.testcases;
 
 
+import org.testng.ITestContext;
 import org.testng.annotations.Test;
 import com.framework.pages.BaseClass;
 import com.framework.pages.HomePage;
@@ -8,11 +9,10 @@ import com.framework.pages.LoginPage;
 
 public class LoginToAppTest extends BaseClass{
     @Test
-    public void loginApp() throws InterruptedException{
+    public void loginApp(ITestContext testContext) throws InterruptedException{
 
         LoginPage lpage = new LoginPage(driver);
-
-        logger = report.createTest("Login To CRMPro");
+        logger = report.createTest(testContext.getName());
         logger.info("Starting Application");
 
         lpage.loginToApp(excel.getStringData("Sheet1", 0, 0), excel.getStringData("Sheet1", 0, 1));
